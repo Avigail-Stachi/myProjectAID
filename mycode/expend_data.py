0eaf1aa83427a467d3430b24719883593d995632
Avigail_Stachi <a0527102693@gmail.com>
2025-05-08 14:52:13 +0300

    expend and clean dataset of texts

--- mycode/expend_data.py
+++ mycode/expend_data.py
@@ -2,12 +2,13 @@ import pandas as pd
 import nltk
 import sys  # לנתיב של EDA
 
-INPUT_FILE = r"C:\project\projectAID\data\data_not_updated\emergency_cases_label_amb_fix_updated.csv"
-OUTPUT_FILE = r"C:\project\projectAID\data\emergency_cases_expend7.csv"
+INPUT_FILE = "../data/emergency_cases_clean5.csv"
+OUTPUT_FILE = "../data/emergency_cases_clean5_expend_7.csv"
 # כמה גרסאות לכל טקסט
 NUM_AUG = 7
 EDA_PATH = r"C:\project\projectAID\exsist"
 sys.path.append(EDA_PATH)
+
 from eda import eda
 
 # הורדת משאבי WordNet רק פעם אחת
@@ -24,26 +25,23 @@ def augment_dataset(input_file, output_file, num_aug=3, eda_path=None):
     df = pd.read_csv(input_file)
 
     augmented_rows = []
-
+    columns = df.columns
     for idx, row in df.iterrows():
         text = row['text']
-        label = row['label']
-        need_ambulance = row['need_ambulance']
+
 
         # הרחבת הטקסט בעזרת EDA
         augmented_texts = augment_text(text, num_aug)
 
         # שמירת הדוגמאות המורחבות
         for new_text in augmented_texts:
-            augmented_rows.append({
-                'text': new_text,
-                'label': label,
-                'need_ambulance': need_ambulance
-            })
+            augmented_row = {col: row[col] for col in columns}
+            augmented_row['text'] = new_text
+            augmented_rows.append(augmented_row)
 
     # שילוב הדאטה המקורי עם המורחב
     aug_df = pd.concat([df, pd.DataFrame(augmented_rows)], ignore_index=True)
-
+    aug_df.drop_duplicates(subset='text', inplace=True)
     # שמירה לקובץ חדש
     aug_df.to_csv(output_file, index=False)
     print(f" הקובץ נשמר בהצלחה בשם: {output_file}")
